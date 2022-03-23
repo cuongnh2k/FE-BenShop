655962993ce7860a637d0f29093e263b7b440d95
cuongnh2k <cuongnh2k@gmail.com>
2022-03-23 16:37:48 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import Footer from "./components/Footer";
 import {Route, Routes} from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Account from "./pages/Account";
 
 const App = () => {
     return <>
         <Header/>
         <Routes>
             <Route path='/' element={<Home/>}/>
+            <Route path='/product-detail' element={<ProductDetail/>}/>
+            <Route path='/product' element={<Product/>}/>
+            <Route path='/cart' element={<Cart/>}/>
+            <Route path='/account' element={<Account/>}/>
         </Routes>
         <Footer/>
     </>
