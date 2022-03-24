8d0e2fe11ae7dff63fd40e9c6ea0be8a778c2a5b
cuongnh2k <cuongnh2k@gmail.com>
2022-03-24 22:40:08 +0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import Slide from "../components/Slide";
 import ProductNew from "../components/ProductNew";
 
 const Home = () => {
-    alert('Website này tạo ra nhằm mục đích thực hành lại kiến thức đã học')
     document.title='Trang chủ'
     return <main style={{minHeight: 1000}}>
         <Slide/>
