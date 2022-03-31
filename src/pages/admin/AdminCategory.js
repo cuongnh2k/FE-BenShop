dc63c05c8b7613b3849549811c722c9d075d4d1f
cuongnh2k <cuongnh2k@gmail.com>
2022-04-01 03:18:54 +0700

    update

--- src/pages/admin/AdminCategory.js
+++ src/pages/admin/AdminCategory.js
@@ -1,6 +1,55 @@
+import AdminHeader from "../../components/admin/AdminHeader";
+import {useEffect, useState} from "react";
+import BasicApi from "../../api/BasicApi";
+import jwt_decode from "jwt-decode";
+import Domain from "../../api/Domain";
+
 const AdminCategory = () => {
+
+    const [category, setCategory] = useState({message: null, success: null, data: []})
+
+    const check_arr = (arr) => {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === 'ROLE_ADMIN') {
+                return true
+            }
+        }
+        return false
+    }
+
+    if (localStorage.getItem('accessToken') == null) {
+        window.location = Domain + '/login'
+    } else {
+        if (!check_arr(jwt_decode(localStorage.getItem('accessToken')))) {
+            window.location = Domain + '/login'
+        }
+    }
+
+    useEffect(() => {
+        fetch(BasicApi.getAllCategory('structure=true').url)
+            .then((res) => res.json())
+            .then((o) => setCategory(o));
+    }, []);
+
     return <>
-        category
+        <AdminHeader/>
+        <main style={{marginTop: 120}}>
+            {category.data.map(o =>
+                <ul key={o.id} style={{lineHeight: 2}}>
+                    <li>{o.name}</li>
+                    {o.categories1.map(oo =>
+                        <ul key={oo.id}>
+                            <li>{oo.name}</li>
+                            {oo.categories2.map(ooo =>
+                                <ul key={ooo.id}>
+                                    <li>{ooo.name}</li>
+                                </ul>
+                            )}
+                        </ul>
+                    )}
+                </ul>
+            )}
+        </main>
     </>
 }
 export default AdminCategory
---
+++ src/pages/admin/AdminUser.js
@@ -0,0 +1,6 @@
+const AdminUser = () => {
+    return <>
+        admin user
+    </>
+}
+export default AdminUser
